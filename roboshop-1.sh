59ec161766374d914a0061666fed37f4ec1021c4
danakamalakar-t <danakamalakar.t@gmail.com>
2026-02-08 23:44:34 +0530

    Roboshop project using Shell Scripting

--- roboshop-1.sh
+++ roboshop-1.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-08ae1eafe91ce777b" # replace with your
+ZONE_ID="Z0417044F9DPXHHFJ4E9" # replace with your ID
+DOMAIN_NAME="danakamalakar.store"
 
 for instance in $@ # mongodb redis mysql
 do
@@ -11,10 +13,29 @@ do
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         echo "$instance: $IP (Private IP)"
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.danakamalakar.store
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         echo "$instance: $IP (Public IP)"
+        RECORD_NAME="$DOMAIN_NAME" # danakamalakar.store
     fi
 
+     aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '{
+        "Comment": "Update A record",
+        "Changes": [{
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+        "Name": "$RECORD_NAME",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          { "Value": "$IP" }
+        ]
+      }
+    }]
+  }'
+   
    
 done
