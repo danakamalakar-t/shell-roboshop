7db4368bf1b9d1f9f133ef8c5883472dd8950f56
danakamalakar-t <danakamalakar.t@gmail.com>
2026-03-17 18:49:54 +0530

    Roboshop project using Shell Scripting

--- set.sh
+++ set.sh
@@ -6,6 +6,8 @@ error_handler() {
     echo "❌ Error occurred!"
     echo "Line: $1"
     echo "Command: $2"
+    echo "Exiting with status: $?"
+    exit 1
 }
 
 trap 'error_handler $LINENO "$BASH_COMMAND"' ERR
